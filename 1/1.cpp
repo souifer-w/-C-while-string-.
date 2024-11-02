f21228cb72d7e6d3c70cdab718eae6a9dd02a683
HP <HP@DESKTOP-NMBILJD>
2024-11-02 16:45:00 +0100

    Add project files.

---
+++ 1/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input;
+    while (input != "s")
+    {
+
+        cin >> input;
+
+    }
+    cout << input << endl;
+
+    return 0;
+}
+// taht was mean you should use string because " exite " is string ;
